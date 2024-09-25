f03daacd0c32447023dcad7d68fac8df28a8c55b
Gabriel Soares <gabrielrs.job@gmail.com>
2024-09-24 21:07:24 -0300

    chore: explicação secret

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,3 +1,4 @@
 export const jwtConstants = {
+  // Retirar o secret do código, apenas foi mantido para fins de demonstração.
   secret: '117f9bf927fc8ce2856b699644a3740a3e32ed1696dbb11fe0214c7911771f71',
 };
